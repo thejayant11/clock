e12b9148b469e9f3399b2e6cd4d36b29f49bc280
Jayant kushwaha <kushwahajayant92@gmail.com>
2026-06-02 14:01:32 +0530

    Clock one feature Create

---
+++ app.js
@@ -0,0 +1,11 @@
+let hrs = document.getElementById('hrs');
+let mins = document.getElementById('min');
+let sec = document.getElementById('sec');
+
+setInterval(()=>{
+    let currentTime = new Date();
+
+    hrs.innerHTML=(currentTime.getHours());
+    mins.innerHTML = (currentTime.getMinutes());
+    sec.innerHTML = (currentTime.getSeconds());
+},1000);
